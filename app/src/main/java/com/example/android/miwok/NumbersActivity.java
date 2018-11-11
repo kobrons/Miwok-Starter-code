a06b08ac233607a734f880d24ad12203f6a772e9
Kobrons <kommandantbrons@gmail.com>
2018-11-11 22:10:58 +0100

    now with ArrayLists instead of Arrays

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,8 +12,27 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        // -Strings in Array schreiben-
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        for (int i = 0;  i < 10; i++){
+            Log.v("Numbers Activity", "Words at index " + i + ": " + words.get(i));
+        }
     }
 
-    String[] words = new String[10];
-    words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+
+
+
+
 }
