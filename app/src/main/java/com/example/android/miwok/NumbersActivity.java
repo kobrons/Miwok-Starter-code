695b53c437ce7ee209e70ffaf32f77e914ea5a1c
Kobrons <kommandantbrons@gmail.com>
2018-11-14 21:58:04 +0100

    numbers now works. rudimentary

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,6 +31,14 @@ public class NumbersActivity extends AppCompatActivity {
         for (int i = 0;  i < 10; i++){
             Log.v("Numbers Activity", "Words at index " + i + ": " + words.get(i));
         }
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        for (int i = 0; i < words.size(); i++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
     }
 
 
